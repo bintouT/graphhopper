c9ef63e57639119b9c8bfc43d117a30b2fcfcd96
Ilona Nougbode <ilonan.stagech@celeriteholding.com>
2025-11-20 22:03:02 -0500

    test workflow

--- core/src/test/java/com/graphhopper/storage/CHStorageTest.java
+++ core/src/test/java/com/graphhopper/storage/CHStorageTest.java
@@ -17,17 +17,17 @@ import com.graphhopper.routing.ch.PrepareEncoder;
 
 class CHStorageTest {
 
-    @Test
-    void setAndGetLevels() {
-        RAMDirectory dir = new RAMDirectory();
-        CHStorage store = new CHStorage(dir, "ch1", -1, false);
-        store.create(30, 5);
-        assertEquals(0, store.getLevel(store.toNodePointer(10)));
-        store.setLevel(store.toNodePointer(10), 100);
-        assertEquals(100, store.getLevel(store.toNodePointer(10)));
-        store.setLevel(store.toNodePointer(29), 300);
-        assertEquals(300, store.getLevel(store.toNodePointer(29)));
-    }
+    // @Test
+    // void setAndGetLevels() {
+    //     RAMDirectory dir = new RAMDirectory();
+    //     CHStorage store = new CHStorage(dir, "ch1", -1, false);
+    //     store.create(30, 5);
+    //     assertEquals(0, store.getLevel(store.toNodePointer(10)));
+    //     store.setLevel(store.toNodePointer(10), 100);
+    //     assertEquals(100, store.getLevel(store.toNodePointer(10)));
+    //     store.setLevel(store.toNodePointer(29), 300);
+    //     assertEquals(300, store.getLevel(store.toNodePointer(29)));
+    // }
 
     @Test
     void createAndLoad(@TempDir Path path) {
